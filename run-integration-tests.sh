d232289266e97a7a1baf989f40949702756cf8e5
erlingrj <erling.jellum@gmail.com>
2023-06-13 16:57:43 +0200

    Do standalone and codesign integration-tests

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
 # List of strings to iterate through
-tests=("TopReactorEx")
+tests_codesign=("TopReactorEx")
+tests_standalone=("ReactorCounter")
 
 make clean
-for test in "${tests[@]}"
+for test in "${tests_standalone[@]}"
+do
+  make run TOP="$test" PROGRAM_TYPE=standalone
+  if [ $? -ne 0 ]; then
+    echo "-----------------------------------------------------------"
+    echo "Integration-test FAILED!"
+    echo "FAILING TEST: $test"
+    echo "-----------------------------------------------------------"
+    exit 1
+  fi
+  make clean
+done
+
+for test in "${tests_codesign[@]}"
 do
   make run TOP="$test" PROGRAM_TYPE=codesign
   if [ $? -ne 0 ]; then
@@ -19,7 +33,8 @@ done
 
 echo "-----------------------------------------------------------"
 echo "Integration-test SUCCEEDED!"
-echo "PASSING TESTS: $tests"
+echo "PASSING STANDALONE TESTS: ${tests_standalone[@]}"
+echo "PASSING CODESIGN TESTS: ${tests_codesign[@]}"
 echo "-----------------------------------------------------------"
 
 
