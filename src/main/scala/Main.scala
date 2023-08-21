950b2d47288fcef648a42682486ca1d41d7614b4
Erling Rennemo Jellum <erling.r.jellum@ntnu.no>
2023-08-21 10:51:58 +0200

    Add support for quick characterization of the generated circuit using oh-my-xilinx

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,12 @@ package reactor
 import chisel3._
 import chisel3.experimental.BundleLiterals._
 import chisel3.util._
-
 import reactor.examples._
+import scala.sys.process.Process
+import java.nio.file.{Files, Paths}
+import fpgatidbits.PlatformWrapper.VerilatedTesterWrapper
+import fpgatidbits.TidbitsMakeUtils._
 import fpgatidbits.PlatformWrapper._
-
-import java.nio.file.Paths
 
 object Settings {
   def writeVerilogToFile(verilog: String, path: String) = {
@@ -23,3 +24,48 @@ object Settings {
     writer.close()
   }
 }
+ object ReactorChisel {
+   def mainStandalone(mainReactorFunc: () => Reactor, targetDir: String, binDir: String, globalCfg: GlobalReactorConfig): Unit = {
+     println("------------------------------------------------------------------------------------------------------")
+     println("Running Chisel compiler to generate verilator project")
+     println("------------------------------------------------------------------------------------------------------")
+     val chiselArgs = Array("--target-dir", s"$targetDir/tmp")
+     val verilog = (new chisel3.stage.ChiselStage).emitVerilog(new StandaloneTopReactor((mainReactorFunc))(globalCfg), chiselArgs)
+     val saveLocation = targetDir + "/ReactorChisel.v"
+     Settings.writeVerilogToFile(verilog, saveLocation)
+     println(s"Wrote the generated verilog to `$saveLocation`")
+     // Copy main cpp file for emulation
+     resourceCopyBulk("simulator/standalone", targetDir, Seq("main.cpp", "Makefile"))
+     println("------------------------------------------------------------------------------------------------------")
+     println("Building verilator project")
+     println("------------------------------------------------------------------------------------------------------")
+     val result = Process("make", new java.io.File(targetDir)).!
+     println("------------------------------------------------------------------------------------------------------")
+     println("Copying executable into bin dir. ")
+     println("------------------------------------------------------------------------------------------------------")
+     if (!Files.exists(Paths.get(binDir))) {
+       Files.createDirectories(Paths.get(binDir))
+     }
+     fpgatidbits.TidbitsMakeUtils.fileCopy(targetDir + "/emu", binDir + "/Timer")
+     println("------------------------------------------------------------------------------------------------------")
+     println("To execute program run:`" + binDir + "/Timer`")
+     println("------------------------------------------------------------------------------------------------------")
+   }
+
+   def mainCodesign(mainReactorFunc: () => Reactor, mainReactorSwIOFunc: () => SwIO, targetDir: String, binDir: String, globalConfig: GlobalReactorConfig) = {
+     println("------------------------------------------------------------------------------------------------------")
+     println("Running Chisel compiler to generate verilator project")
+     println("------------------------------------------------------------------------------------------------------")
+     val chiselArgs = Array("--target-dir", s"$targetDir")
+     (new chisel3.stage.ChiselStage).emitVerilog(new VerilatedTesterWrapper(_ => new CodesignTopReactor(mainReactorFunc, mainReactorSwIOFunc)(globalConfig), targetDir), chiselArgs)
+     // Copy main cpp file for emulation
+     resourceCopyBulk("simulator/codesign", targetDir, Seq("main.cpp", "Makefile"))
+     println("------------------------------------------------------------------------------------------------------")
+     println("Building verilator project")
+     println("------------------------------------------------------------------------------------------------------")
+     val result = Process("make lib", new java.io.File(targetDir)).!
+     println("------------------------------------------------------------------------------------------------------")
+     println(s"Library produced to $targetDir/lfFPGA.a.")
+     println("------------------------------------------------------------------------------------------------------")
+   }
+ }
--- src/main/scala/Utils.scala
+++ src/main/scala/Utils.scala
@@ -2,6 +2,10 @@ package reactor.util
 
 import chisel3._
 import chisel3.util._
+import reactor._
+import fpgatidbits.synthutils.VivadoSynth
+import fpgatidbits.synthutils.PrintableParam
+import reactor.util.CharacterizeUtils.DummyParams
 
 import Numeric._
 import scala.collection.mutable.ArrayBuffer
@@ -9,4 +13,25 @@ import scala.collection.mutable.ArrayBuffer
 
 object ReactorFault extends ChiselEnum {
   val None, ReactionTimeout, MemoryFault = Value
+}
+
+object CharacterizeUtils {
+  class DummyParams extends PrintableParam {
+    def headersAsList(): List[String] = List("")
+    def contentAsList(): List[String] = List("")
+  }
+  val dummyParam = new DummyParams
+  val zedboardPart = "xc7z020clg484-1"
+
+  def standalone(reactor: () => StandaloneTopReactor, buildDir: String): Unit = {
+    val genTop  = (d: DummyParams) => reactor.apply()
+    VivadoSynth.characterizePoint(dummyParam, genTop, buildDir, zedboardPart,"StandaloneTopReactor")
+  }
+
+  def codesign(reactor: () => CodesignTopReactor, buildDir: String): Unit = {
+    val genTop = (d: DummyParams) => reactor.apply()
+    VivadoSynth.characterizePoint(dummyParam, genTop, buildDir, zedboardPart, "CodesignTopReactor")
+
+  }
+
 }
